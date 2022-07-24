358e724f908477023636b67eb0c9413e1c763760
Michael Reinhart <michaelreinhart@Macbook-Michael.local>
2022-07-24 18:50:06 -0400

    rename deployment

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.get("/", function(req, res){
 app.get("/results", function(req, res){
     var movieName = req.query.movieName;
     var url = "http://omdbapi.com/?s=" + movieName + "&apikey=thewdb"
-    console.log(movieName)
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var results = JSON.parse(body);
